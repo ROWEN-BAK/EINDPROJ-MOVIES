d250a8b7ad904f4ad248b8c720313ca9b31cac3f
Rowen Bakkeren <9018934@student.zadkine.nl>
2025-03-27 11:17:08 +0100

    man

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -3,7 +3,7 @@ const Catalog = () => {
 <section className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center text-center p-6">
   <h1 className="text-4xl md:text-5xl font-bold mb-4">Onze Catalogus</h1>
   <p className="text-lg md:text-xl max-w-2xl">
-   Op deze pagina kunt uw onze prachtige en <span className="text-yellow-400 font-semibold">sappige </span>
+   Op deze pagina kunt uw onze prachtige en <span className="text-red-400 font-semibold">sappige </span>
     catalogus van films inzien.
   </p>
 </section>
