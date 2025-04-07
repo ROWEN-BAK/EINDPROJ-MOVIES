55b50019a8b8c707975f2fb423db8a7fb73309b8
MarkvanderGiessen <9020308@student.zadkine.nl>
2025-04-07 14:24:28 +0200

    Delete NoPage.jsx

--- frontend/src/pages/NoPage.jsx
+++
@@ -1,9 +0,0 @@
-const NoPage = () => {
-    return ( 
-        <section>
-            <h1>404 - Page Not Found. Please return to the last page and try again.</h1>
-        </section>
-     );
-}
- 
-export default NoPage;
